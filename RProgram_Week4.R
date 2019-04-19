03ecdf8f0806088de310fb6459e23da82e66135d
Cammillius Jeyadev <cammillius.rj@nabler.com>
2019-04-19 16:59:30 +0530

    message

---
+++ RProgram_Week4.R
@@ -0,0 +1,12 @@
+# str Function 
+# It compactly displays the internal structure of an R object 
+# A diagnostic function and an alternative to R 'summary' 
+# It is especially well suited to compactly display the (abbreviated) contents of (possibly nested) lists. 
+# Rough one line per basic object 
+str(str)
+str(lm)
+str(ls)
+x <- rnorm(100, 2, 4)
+summary(x)
+str(x)
+f 

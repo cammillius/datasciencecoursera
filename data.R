730580637c3453782af0ac755c6268d0d040a871
cammillius <camjey@gmail.com>
2019-04-28 20:57:49 +0530

    m

--- data.R
+++
@@ -1,5 +0,0 @@
-x <-
-"foo"
-y <-
-structure(list(s = 1, b = structure(1L, .Label = "a", class = "factor")), class = "data.frame", row.names = c(NA, 
--1L))
--- y.R
+++
@@ -1,2 +0,0 @@
-structure(list(a = 1, b = structure(1L, .Label = "string", class = "factor")), class = "data.frame", row.names = c(NA, 
--1L))
